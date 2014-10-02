def801dbd9777ad4e4b6c629748bd4c4dde17ce1
kepbautista@gmail.com <kepbautista@gmail.com@91e435a8-72b7-ca0c-145b-2dcf1bd872f0>
2014-10-02 15:56:54 +0000

    addslashes for input query

---  regie-babies/processing.php
+++  regie-babies/processing.php
@@ -1,7 +1,22 @@
 <?php
- $query=$_POST['query'];
+//classes
+include("parser.php");
 
-exec('./oursql "'.$query.'"',$output);
+//instantiate classes
+$parse = new Parser();//parser class
 
-print_r($output);
+$query=addslashes($_POST['query']);
+echo $query."\n";
+
+/*add double quotes at the start & end of string
+  for passing to command line arguments in the executable
+  files
+*/
+$query=$parse->addDoubleQuotes($query);
+
+exec("./oursql {$query}",$out1);//run C++ program
+exec("java Echo {$query}",$out2);//run Java program
+
+print_r($out1);
+print_r($out2);
 ?>
